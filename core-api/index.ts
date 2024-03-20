a799250e1ef6390bb7887d574ee624184b031738
Kevin Anderson <kevin@seis.com>
2024-03-20 08:41:24 -0600

    fix imports

--- core-api/index.ts
+++ core-api/index.ts
@@ -2,5 +2,5 @@ export {NetworkPayment} from "./networkPayments/NetworkPayment";
 export {NetworkPaymentStatus} from "./networkPayments/NetworkPaymentStatus";
 export {NetworkUserOptions} from "./networkPayments/NetworkUserOptions";
 export {ListOptions} from './common/common';
-export {CreditCardAccount, CreditCardAccountStatus, CreateCreditCardAccountInput, FindCreditCardAccountInput} from './credit-cards/accounts';
+export {CreditCardAccount, CreditCardAccountStatus, CreateCreditCardAccountInput, FindCreditCardAccountInput, CreditCardAccountScheduledPaymentAmountType, PatchCreditCardAccountInput} from './credit-cards/accounts';
 export {CreditCardPayment, CreditCardPaymentStatus, CreateCreditCardPaymentInput, GetCreditCardPaymentInput, FindCreditCardPaymentInput} from './credit-cards/payments';
