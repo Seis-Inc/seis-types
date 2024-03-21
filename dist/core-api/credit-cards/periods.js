bd0aa89a5bff08d1b42e0cdaf3d8df12f7e67ecc
Kevin Anderson <kevin@seis.com>
2024-03-21 02:29:39 -0600

    adding periods

---
+++ dist/core-api/credit-cards/periods.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreditCardAccountPeriodStatus = void 0;
+var CreditCardAccountPeriodStatus;
+(function (CreditCardAccountPeriodStatus) {
+    CreditCardAccountPeriodStatus["NotDue"] = "NotDue";
+    CreditCardAccountPeriodStatus["Due"] = "Due";
+    CreditCardAccountPeriodStatus["Failed"] = "Failed";
+    CreditCardAccountPeriodStatus["Paid"] = "Paid";
+    CreditCardAccountPeriodStatus["PastDue"] = "PastDue";
+})(CreditCardAccountPeriodStatus || (exports.CreditCardAccountPeriodStatus = CreditCardAccountPeriodStatus = {}));
+//# sourceMappingURL=periods.js.map
--- dist/core-api/index.js
+++ dist/core-api/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreditCardPaymentStatus = exports.CreditCardAccountScheduledPaymentType = exports.CreditCardAccountScheduledPaymentAmountType = exports.CreditCardAccountStatus = exports.NetworkUserOptions = exports.NetworkPaymentStatus = void 0;
+exports.CreditCardAccountPeriodStatus = exports.CreditCardPaymentStatus = exports.CreditCardAccountScheduledPaymentType = exports.CreditCardAccountScheduledPaymentAmountType = exports.CreditCardAccountStatus = exports.NetworkUserOptions = exports.NetworkPaymentStatus = void 0;
 var NetworkPaymentStatus_1 = require("./networkPayments/NetworkPaymentStatus");
 Object.defineProperty(exports, "NetworkPaymentStatus", { enumerable: true, get: function () { return NetworkPaymentStatus_1.NetworkPaymentStatus; } });
 var NetworkUserOptions_1 = require("./networkPayments/NetworkUserOptions");
@@ -11,4 +11,6 @@ Object.defineProperty(exports, "CreditCardAccountScheduledPaymentAmountType", { 
 Object.defineProperty(exports, "CreditCardAccountScheduledPaymentType", { enumerable: true, get: function () { return accounts_1.CreditCardAccountScheduledPaymentType; } });
 var payments_1 = require("./credit-cards/payments");
 Object.defineProperty(exports, "CreditCardPaymentStatus", { enumerable: true, get: function () { return payments_1.CreditCardPaymentStatus; } });
+var periods_1 = require("./credit-cards/periods");
+Object.defineProperty(exports, "CreditCardAccountPeriodStatus", { enumerable: true, get: function () { return periods_1.CreditCardAccountPeriodStatus; } });
 //# sourceMappingURL=index.js.map
