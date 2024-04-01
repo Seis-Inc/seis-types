eb8f4ba5a42c138d587b778683c034f733e99475
tannerburton <tanner@seis.com>
2024-04-01 15:31:28 -0600

    export type, new version

--- dist/ledger-service/index.ts
+++ dist/ledger-service/index.ts
@@ -1,2 +1,3 @@
 export {TransactionEventInput} from "./TransactionEventInput";
 export {AuthorizationCreated} from "./AuthorizationCreated";
+export {AuthorizationRequested} from "./AuthorizationRequested";
