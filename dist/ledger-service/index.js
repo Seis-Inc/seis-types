86169abf292b73a9174ce6221751dc93216af9da
tannerburton <tanner@seis.com>
2024-04-02 11:09:23 -0600

    export new types

--- dist/ledger-service/index.js
+++ dist/ledger-service/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthorizationEventType = void 0;
+var AuthorizationEventType_1 = require("./AuthorizationEventType");
+Object.defineProperty(exports, "AuthorizationEventType", { enumerable: true, get: function () { return AuthorizationEventType_1.AuthorizationEventType; } });
 //# sourceMappingURL=index.js.map
