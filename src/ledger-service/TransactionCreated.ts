075d5101ed115a35ce6cb4c5ae0e6e6f7e521d53
tannerburton <tanner@seis.com>
2024-04-10 13:23:32 -0600

    update TransactionCreated.ts event to allow card Ids

--- src/ledger-service/TransactionCreated.ts
+++ src/ledger-service/TransactionCreated.ts
@@ -2,7 +2,9 @@ type TransactionType = 'Payment' | 'Interest' | 'CardTransaction'
 
 export type TransactionCreated = {
   fromAccountId: string,
+  fromCardId: string | null,
   toAccountId: string,
+  toCardId: string | null,
   amountCents: number,
   description: string,
   merchantLocation: string | null,
