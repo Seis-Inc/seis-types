d3bb19b654ad1ca54347c08f0bb25704ae4d69d2
tannerburton <tanner@seis.com>
2024-04-11 13:51:42 -0600

    added properties to authorizations

--- dist/ledger-service/index.js
+++ dist/ledger-service/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthorizationEventType = void 0;
+exports.AuthorizationType = exports.AuthorizationEventType = void 0;
 var AuthorizationEventType_1 = require("./AuthorizationEventType");
 Object.defineProperty(exports, "AuthorizationEventType", { enumerable: true, get: function () { return AuthorizationEventType_1.AuthorizationEventType; } });
+var AuthorizationType_1 = require("./AuthorizationType");
+Object.defineProperty(exports, "AuthorizationType", { enumerable: true, get: function () { return AuthorizationType_1.AuthorizationType; } });
 //# sourceMappingURL=index.js.map
