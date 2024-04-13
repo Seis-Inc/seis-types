cc1a14f9d321784f95824ed6d5a4dc7e1981333e
Kevin Anderson <kevin@seis.com>
2024-04-13 08:48:29 -0600

    formatting

---
+++ dist/core-api/credit-cards/secured-accounts.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreditCardSecuredAccountStatus = void 0;
+var CreditCardSecuredAccountStatus;
+(function (CreditCardSecuredAccountStatus) {
+    CreditCardSecuredAccountStatus["Open"] = "Open";
+    CreditCardSecuredAccountStatus["Closed"] = "Closed";
+    CreditCardSecuredAccountStatus["Frozen"] = "Frozen";
+})(CreditCardSecuredAccountStatus || (exports.CreditCardSecuredAccountStatus = CreditCardSecuredAccountStatus = {}));
+//# sourceMappingURL=secured-accounts.js.map
---
+++ dist/core-api/credit-cards/secured-accounts.ts
@@ -0,0 +1,31 @@
+import { ListOptions } from "../common/ListOptions";
+import { Page } from "../common/Page";
+
+export enum CreditCardSecuredAccountStatus {
+    Open = 'Open',
+    Closed = 'Closed',
+    Frozen = 'Frozen'
+}
+
+export type CreditCardSecuredAccount = {
+    id: string;
+    creditCardAccountId: string;
+    accountNumber: string;
+    status: CreditCardSecuredAccountStatus;
+    balanceCents: number;
+    holdCents: number;
+    availableCents: number;
+    createdAt: string;
+    updatedAt: string;
+};
+
+export type CreateCreditCardSecuredAccountInput = Omit<CreditCardSecuredAccount, 'id' | 'accountNumber' |'createdAt' | 'updatedAt'>;
+export type PatchCreditCardSecuredAccountInput = Partial<CreateCreditCardSecuredAccountInput>;
+
+export type CreditCardSecuredAccountFilter = {
+    id?: string;
+    creditCardAccountId?: string;
+    status?: CreditCardSecuredAccountStatus;
+} & ListOptions
+
+export type CreditCardSecuredAccountPage = Page<CreditCardSecuredAccount>;
