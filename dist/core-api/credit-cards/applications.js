f3dffeb84ed6a7fec84d36f908a5068ee9a3a3a4
Derrick Hathaway <derr@me.com>
2024-04-15 16:35:43 -0600

    Adds CC Application Progress & Status

--- dist/core-api/credit-cards/applications.js
+++ dist/core-api/credit-cards/applications.js
@@ -1,3 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreditCardApplicationStatus = exports.CreditCardApplicationProgress = void 0;
+var CreditCardApplicationProgress;
+(function (CreditCardApplicationProgress) {
+    CreditCardApplicationProgress["NotStarted"] = "NotStarted";
+    CreditCardApplicationProgress["IncomeAndExpensesConfirmed"] = "IncomeAndExpensesConfirmed";
+    CreditCardApplicationProgress["ApplicationConsentGranted"] = "ApplicationConsentGranted";
+    CreditCardApplicationProgress["DisclosuresAccepted"] = "DisclosuresAccepted";
+})(CreditCardApplicationProgress || (exports.CreditCardApplicationProgress = CreditCardApplicationProgress = {}));
+var CreditCardApplicationStatus;
+(function (CreditCardApplicationStatus) {
+    CreditCardApplicationStatus["Pending"] = "Pending";
+    CreditCardApplicationStatus["Approved"] = "Approved";
+    CreditCardApplicationStatus["Declined"] = "Declined";
+})(CreditCardApplicationStatus || (exports.CreditCardApplicationStatus = CreditCardApplicationStatus = {}));
 //# sourceMappingURL=applications.js.map
