ea8f36b9459ea0ded3175d0c0f593d01acd45f30
Matt McMurry <matt@seis.com>
2024-04-18 15:11:02 -0600

    add token to serach filter for credit cards (build)

--- dist/core-api/credit-cards/CreditCardFilter.ts
+++ dist/core-api/credit-cards/CreditCardFilter.ts
@@ -1,3 +1,3 @@
 import {ListOptions} from "../common/ListOptions";
 
-export type CreditCardFilter = {userId?: string} & ListOptions;
+export type CreditCardFilter = {userId?: string, token?: string} & ListOptions;
