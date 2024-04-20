00f229c8f191b9788c9b164b39efd3156fe70560
Kevin Anderson <kevin@seis.com>
2024-04-20 04:26:52 -0600

    creating new version

--- dist/core-api/credit-cards/CreditCardInput.ts
+++ dist/core-api/credit-cards/CreditCardInput.ts
@@ -1,4 +1,4 @@
 import { CreditCard } from "./CreditCard";
 
-export type CreditCardInput = Omit<CreditCard, 'id' | 'createdAt' | 'updatedAt'>;
+export type CreditCardInput = Omit<CreditCard, 'id' | 'createdAt' | 'updatedAt' | 'activatedAt' | 'canceledAt' | 'cancelationReason'>;
 
