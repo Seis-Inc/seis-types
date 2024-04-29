8155303af715ffa8031425381923f83fcbeb5f7a
Matt McMurry <matt@seis.com>
2024-04-29 12:00:05 -0600

    remote the cc application step type

--- src/core-api/credit-cards/applications.ts
+++ src/core-api/credit-cards/applications.ts
@@ -30,15 +30,6 @@ export type CreditCardApplicationDecision = {
   updatedAt: string;
 }
 
-export enum CreditCardCustomerApplicationStep {
-  InputIncomeAndExpenses = 'InputIncomeAndExpenses',
-  AcceptConsent = 'AcceptConsent',
-  AwaitReview = 'AwaitReview',
-  AcceptDisclosures = 'AcceptDisclosures',
-  Approved = 'Approved',
-  Denied = 'Denied'
-}
-
 export interface CreditCardApplication {
   id: string
   userId: string
