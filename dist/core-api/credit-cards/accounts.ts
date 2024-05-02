a8a53ab7179062541ea4885d8d9be7d779da7962
Kevin Anderson <kevin@seis.com>
2024-05-02 07:57:07 -0600

    creating new version

--- dist/core-api/credit-cards/accounts.ts
+++ dist/core-api/credit-cards/accounts.ts
@@ -1,11 +1,11 @@
-import {ListOptions} from "../common/ListOptions";
+import { ListOptions } from "../common/ListOptions";
 import { Optional } from "../common/Optional";
 import { CreditCardSecuredAccount } from "./secured-accounts";
 
 export enum CreditCardAccountStatus {
-    Open = 'Open',
-    Closed = 'Closed',
-    Frozen = 'Frozen'
+	Open = 'Open',
+	Closed = 'Closed',
+	Frozen = 'Frozen'
 }
 
 export enum CreditCardAccountScheduledPaymentAmountType {
@@ -17,25 +17,26 @@ export enum CreditCardAccountScheduledPaymentAmountType {
 }
 
 export enum CreditCardAccountScheduledPaymentType {
-  ScheduledOnDueDate = 'ScheduledOnDueDate',
-  ScheduledOnStatementDate = 'ScheduledOnStatementDate'
+	ScheduledOnDueDate = 'ScheduledOnDueDate',
+	ScheduledOnStatementDate = 'ScheduledOnStatementDate'
 }
 
 export type CreditCardAccount = {
-    id: string;
-    userId: string;
-    status: CreditCardAccountStatus;
+	id: string;
+	userId: string;
+	status: CreditCardAccountStatus;
 	unsecuredCreditLimitCents: number;
-    creditLimitCents: number;
-    interestRate: number;
-    balanceCents: number;
-    name: string;
-    autopayConfiguration: CreditCardAccountAutopayConfiguration | null;
-	  lithicAccountToken: string | null;
-	  debtAccountId: string;
-    idempotencyKey: string | null;
-    createdAt: string;
-    updatedAt: string;
+	creditLimitCents: number;
+	availableCreditLimitCents: number;
+	interestRate: number;
+	balanceCents: number;
+	name: string;
+	autopayConfiguration: CreditCardAccountAutopayConfiguration | null;
+	lithicAccountToken: string | null;
+	debtAccountId: string;
+	idempotencyKey: string | null;
+	createdAt: string;
+	updatedAt: string;
 	securedAccount: Optional<CreditCardSecuredAccount>
 };
 
@@ -61,7 +62,7 @@ export enum CreditCardAccountAutopayInterval {
 }
 
 export type CreditCardAccountAutopayConfiguration = {
-  id: string;
+	id: string;
 	creditCardAccountId: string;
 	status: CreditCardAccountAutopayConfigurationStatus;
 	payoffAmountType: CreditCardAccountAutopayPayoffAmountType;
@@ -71,22 +72,22 @@ export type CreditCardAccountAutopayConfiguration = {
 	updatedAt: Date;
 }
 
-export type CreateCreditCardAccountAutopayConfigurationInput = Omit<CreditCardAccountAutopayConfiguration, 'id' | 'creditCardAccountId' |  'createdAt' | 'updatedAt'>;
-export type PatchCreditCardAccountAutopayConfigurationInput = Partial<Omit<CreditCardAccountAutopayConfiguration, 'id' | 'creditCardAccountId' |'createdAt' | 'updatedAt'>>;
+export type CreateCreditCardAccountAutopayConfigurationInput = Omit<CreditCardAccountAutopayConfiguration, 'id' | 'creditCardAccountId' | 'createdAt' | 'updatedAt'>;
+export type PatchCreditCardAccountAutopayConfigurationInput = Partial<Omit<CreditCardAccountAutopayConfiguration, 'id' | 'creditCardAccountId' | 'createdAt' | 'updatedAt'>>;
 
 export type CreateCreditCardAccountInput = Omit<CreditCardAccount, 'id' | 'createdAt' | 'updatedAt' | 'idempotencyKey' | 'autopayConfiguration'>;
 
 export type PatchCreditCardAccountInput = Partial<Omit<CreditCardAccount, 'id' | 'userId' | 'createdAt' | 'updatedAt' | 'idempotencyKey' | 'autopayConfiguration'>>;
 
 export type FindCreditCardAccountInput = {
-  id?: string;
-  userId?: string;
-  autopayAt?: string;
-  balanceCentsGreaterThan?: number;
+	id?: string;
+	userId?: string;
+	autopayAt?: string;
+	balanceCentsGreaterThan?: number;
 	lithicAccountToken?: string;
 } & ListOptions;
 
 export type FilterCreditCardAccountAutopayConfiguration = {
-  id?: string;
-  creditCardAccountId?: string;
+	id?: string;
+	creditCardAccountId?: string;
 } & ListOptions;
