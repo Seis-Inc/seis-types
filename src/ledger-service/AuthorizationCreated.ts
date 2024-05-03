52bb97e46a938724eefb0039dcc7a92100f10519
Kevin Anderson <kevin@seis.com>
2024-05-03 11:28:21 -0600

    creating new version

--- src/ledger-service/AuthorizationCreated.ts
+++ src/ledger-service/AuthorizationCreated.ts
@@ -3,16 +3,16 @@ import {AuthorizationType} from "./AuthorizationType";
 export type AuthorizationCreated = {
   amountCents: number;
   accountId: string;
-  merchantMcc: string;
-  merchantName: string;
+  merchantMcc: string | null;
+  merchantName: string | null;
   merchantAddress: string | null;
   merchantCity: string | null;
   merchantState: string | null;
   merchantZip: string | null;
   merchantCountryCode: string | null;
-  cardId: string;
+  cardId: string | null;
   network: string;
   id: string;
-  acquirerFeeCents:number;
+  acquirerFeeCents:number | null;
   type:AuthorizationType;
 }
