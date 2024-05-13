b0ef8366bcd910aafa20b8977e51f81817ed72c5
tannerburton <tanner@seis.com>
2024-05-13 10:24:02 -0600

    new version

--- dist/ledger-service/index.js
+++ dist/ledger-service/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthorizationType = exports.AuthorizationEventType = void 0;
+exports.AuthorizationType = exports.AuthorizationStatus = exports.AuthorizationEventType = void 0;
 var AuthorizationEventType_1 = require("./AuthorizationEventType");
 Object.defineProperty(exports, "AuthorizationEventType", { enumerable: true, get: function () { return AuthorizationEventType_1.AuthorizationEventType; } });
+var AuthorizationStatus_1 = require("./AuthorizationStatus");
+Object.defineProperty(exports, "AuthorizationStatus", { enumerable: true, get: function () { return AuthorizationStatus_1.AuthorizationStatus; } });
 var AuthorizationType_1 = require("./AuthorizationType");
 Object.defineProperty(exports, "AuthorizationType", { enumerable: true, get: function () { return AuthorizationType_1.AuthorizationType; } });
 //# sourceMappingURL=index.js.map
