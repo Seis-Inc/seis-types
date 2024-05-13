a5ea4ac6c0d0f7a74312d7b3721803bb71bd8c4e
tannerburton <tanner@seis.com>
2024-05-13 11:23:25 -0600

    remove authorizationfilter

--- src/ledger-service/AuthorizationFilter.ts
+++
@@ -1,9 +0,0 @@
-export type AuthorizationFilter = {
-  accountId: string
-  search?: string
-  orderBy?: 'NewestFirst' | 'OldestFirst'
-  createdAfter?: string
-  createdBefore?: string
-  offset?: number
-  limit?: number
-}
