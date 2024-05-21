d0910e2e4527dad5c9ced5982e5a45340c853c4c
Kevin Anderson <kevin@seis.com>
2024-05-21 16:49:55 -0600

    creating new version

--- dist/core-api/credit-cards/secured-accounts.js
+++ dist/core-api/credit-cards/secured-accounts.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreditCardSecuredAccountStatus = void 0;
+exports.CreditCardSecuredAccountTransactionDirection = exports.CreditCardSecuredAccountStatus = void 0;
 var CreditCardSecuredAccountStatus;
 (function (CreditCardSecuredAccountStatus) {
     CreditCardSecuredAccountStatus["Open"] = "Open";
     CreditCardSecuredAccountStatus["Closed"] = "Closed";
     CreditCardSecuredAccountStatus["Frozen"] = "Frozen";
 })(CreditCardSecuredAccountStatus || (exports.CreditCardSecuredAccountStatus = CreditCardSecuredAccountStatus = {}));
+var CreditCardSecuredAccountTransactionDirection;
+(function (CreditCardSecuredAccountTransactionDirection) {
+    CreditCardSecuredAccountTransactionDirection["Debit"] = "Debit";
+    CreditCardSecuredAccountTransactionDirection["Credit"] = "Credit";
+})(CreditCardSecuredAccountTransactionDirection || (exports.CreditCardSecuredAccountTransactionDirection = CreditCardSecuredAccountTransactionDirection = {}));
 //# sourceMappingURL=secured-accounts.js.map
--- src/core-api/credit-cards/secured-accounts.ts
+++ src/core-api/credit-cards/secured-accounts.ts
@@ -22,10 +22,27 @@ export type CreditCardSecuredAccount = {
 export type CreateCreditCardSecuredAccountInput = Omit<CreditCardSecuredAccount, 'id' | 'accountNumber' |'createdAt' | 'updatedAt'>;
 export type PatchCreditCardSecuredAccountInput = Partial<CreateCreditCardSecuredAccountInput>;
 
+export enum CreditCardSecuredAccountTransactionDirection {
+	Debit = 'Debit',
+	Credit = 'Credit'
+}
+
+export type CreditCardSecuredAccountTransaction = {
+	id: string;
+	createdAt: string;
+	direction: CreditCardSecuredAccountTransactionDirection;
+	amountCents: number;
+};
+
 export type CreditCardSecuredAccountFilter = {
     id?: string;
     creditCardAccountId?: string;
     status?: CreditCardSecuredAccountStatus;
 } & ListOptions
+
+export type CreditCardSecuredAccountTransaactions = {
+    transactions: CreditCardSecuredAccountTransaction[];
+    totalAmountCents: number;
+};
 
 export type CreditCardSecuredAccountPage = Page<CreditCardSecuredAccount>;
