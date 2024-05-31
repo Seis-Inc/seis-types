15773492ae866e77357aa7e3a1e89357263f8c0a
Matt McMurry <matt@seis.com>
2024-05-31 16:15:28 -0600

    add card type filter

--- dist/core-api/credit-cards/CreditCardFilter.ts
+++ dist/core-api/credit-cards/CreditCardFilter.ts
@@ -1,4 +1,5 @@
 import {ListOptions} from "../common/ListOptions";
+import {CreditCardType} from "./CreditCardType";
 
-export type CreditCardFilter = {userId?: string, token?: string, creditCardAccountId?: string} & ListOptions;
+export type CreditCardFilter = {userId?: string, token?: string, creditCardAccountId?: string, cardType?: CreditCardType} & ListOptions;
 
