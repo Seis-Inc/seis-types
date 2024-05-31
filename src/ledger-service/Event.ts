6cb0508a4e77f73d723352ade5c621c16be4e1dc
tannerburton <tanner@seis.com>
2024-05-31 16:16:06 -0600

    add transaction to event

--- src/ledger-service/Event.ts
+++ src/ledger-service/Event.ts
@@ -1,10 +1,12 @@
 import {EventData} from "./EventData";
 import {EventType} from "./EventType";
+import {Transaction} from "./Transaction";
 
 export type Event<T> = {
   id: string;
   data: EventData & T;
   type: EventType;
-  accountId:string | null;
+  accountId: string | null;
   createdAt: string;
+  transaction: Transaction | null;
 };
