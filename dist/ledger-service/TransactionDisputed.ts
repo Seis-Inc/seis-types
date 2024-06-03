bcd8f51d605095d926f8d05899eca4b8fe350ef8
Kevin Anderson <kevin@seis.com>
2024-06-03 14:51:15 -0600

    creating new version

--- dist/ledger-service/TransactionDisputed.ts
+++ dist/ledger-service/TransactionDisputed.ts
@@ -1,6 +1,7 @@
 export type TransactionDisputed = {
     id: string,
     disputeId: string,
+    disputeRefNum: string,
     accountId: string,
     disputedAmountCents: number,
     disputedAt: string
