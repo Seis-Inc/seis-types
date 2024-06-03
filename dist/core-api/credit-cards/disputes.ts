a6e559eae8ad930c9ba10c5aabbbee5e2987aafe
Kevin Anderson <kevin@seis.com>
2024-06-03 14:50:41 -0600

    creating new version

--- dist/core-api/credit-cards/disputes.ts
+++ dist/core-api/credit-cards/disputes.ts
@@ -22,11 +22,12 @@ export type CreditCardDispute = {
     provisionalCreditReversalTransactionId: string | null;
     provisionalCreditReversalIssuedAt: string | null;
     resolvedAt: string | null;
+    disputeRefNum: number;
     createdAt: string;
     updatedAt: string;
 }
 
-export type CreateCreditCardDisputeInput = Omit<CreditCardDispute, 'id' | 'createdAt' | 'updatedAt'>;
+export type CreateCreditCardDisputeInput = Omit<CreditCardDispute, 'id' | 'createdAt' | 'updatedAt' | 'disputeRefNum'>;
 
 export type PatchCreditCardDisputeInput = Partial<Omit<CreditCardDispute, 'id' | 'transactionId' | 'creditCardAccountId' | 'createdAt' | 'updatedAt'>>;
 
