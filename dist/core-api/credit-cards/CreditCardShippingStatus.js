2b2f7d517e70e209eeb661db277874cb733e7615
Matt McMurry <matt@seis.com>
2024-08-08 15:14:16 -0600

    moving CreditCardShippingStatus from graphql to here

---
+++ dist/core-api/credit-cards/CreditCardShippingStatus.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreditCardShippingStatus = void 0;
+var CreditCardShippingStatus;
+(function (CreditCardShippingStatus) {
+    CreditCardShippingStatus["Standard"] = "Standard";
+    CreditCardShippingStatus["Express"] = "Express";
+})(CreditCardShippingStatus || (exports.CreditCardShippingStatus = CreditCardShippingStatus = {}));
+//# sourceMappingURL=CreditCardShippingStatus.js.map
