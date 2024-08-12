c4f4ca01334f20cc0d9f80570b9c95e660305af7
Matt McMurry <matt@seis.com>
2024-08-12 14:18:30 -0600

    add to credit card filter

--- dist/core-api/credit-cards/CreditCardFilter.ts
+++ dist/core-api/credit-cards/CreditCardFilter.ts
@@ -1,5 +1,14 @@
 import {ListOptions} from "../common/ListOptions";
 import {CreditCardType} from "./CreditCardType";
+import {CreditCardShippingStatus} from "./CreditCardShippingStatus";
 
-export type CreditCardFilter = {userId?: string, token?: string, creditCardAccountId?: string, cardType?: CreditCardType} & ListOptions;
+export type CreditCardFilter = {
+    userId?: string,
+    token?: string,
+    creditCardAccountId?: string,
+    cardType?: CreditCardType,
+    estimatedDeliveredToHomeAtIsNull?: boolean,
+    shippingStatus?: CreditCardShippingStatus,
+    isNotClosedAndHasShippedWithinLastMonth?: boolean
+} & ListOptions;
 
