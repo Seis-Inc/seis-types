1cbf4bb16dd9de4545c0a7dd274175c2e0abc031
Leibo <ido.leibole@gmail.com>
2024-09-12 20:07:29 +0300

    creating new version

---
+++ dist/ledger-service/AccountInclude.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AccountInclude = void 0;
+var AccountInclude;
+(function (AccountInclude) {
+    AccountInclude["maxBalanceOwedCents"] = "maxBalanceOwedCents";
+})(AccountInclude || (exports.AccountInclude = AccountInclude = {}));
+//# sourceMappingURL=AccountInclude.js.map
---
+++ dist/ledger-service/AccountInclude.ts
@@ -0,0 +1,3 @@
+export enum AccountInclude {
+  maxBalanceOwedCents = "maxBalanceOwedCents",
+}
